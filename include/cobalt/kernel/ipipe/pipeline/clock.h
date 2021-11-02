5d725f46b306c6777f17f0cf29ed2b5e463db6b3
Alexy Debus <alexy.debus@eleves.ec-nantes.fr>
2021-11-02 23:38:03 +0100

    Add some missing files for the kernel compilation
    TODO : Still somme incoherence between the initial repo. for xenomai and the repo for EDF implementation

---
+++ include/cobalt/kernel/ipipe/pipeline/clock.h
@@ -0,0 +1,40 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#ifndef _COBALT_KERNEL_IPIPE_CLOCK_H
+#define _COBALT_KERNEL_IPIPE_CLOCK_H
+
+#include <linux/ipipe_tickdev.h>
+
+struct timespec;
+
+static inline u64 pipeline_read_cycle_counter(void)
+{
+	u64 t;
+	ipipe_read_tsc(t);
+	return t;
+}
+
+static inline void pipeline_set_timer_shot(unsigned long cycles)
+{
+	ipipe_timer_set(cycles);
+}
+
+static inline const char *pipeline_timer_name(void)
+{
+	return ipipe_timer_name();
+}
+
+static inline const char *pipeline_clock_name(void)
+{
+	return ipipe_clock_name();
+}
+
+int pipeline_get_host_time(struct timespec *tp);
+
+void pipeline_update_clock_freq(unsigned long long freq);
+
+void pipeline_init_clock(void);
+
+#endif /* !_COBALT_KERNEL_IPIPE_CLOCK_H */
